244a8933c8f77843d96706d554a5bb090920a629
Sk8alex <excellentforme@gmail.com>
2025-05-25 15:19:23 +0300

    error

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Flask2_250525
+Flask2_25052025
+Развертывание на локальной машине
+Создаем виртуальное окружение: python3 -m venv flask_venv
+Активируем venv: source flask_venv/bin/activate
+Устанавливаем зависимости: pip install -r requirements.txt
+Создаем локальную БД: flask db upgrade
+Запускаем приложение: python run.py
+Ссылка на документацию
+Настройки для запуска flask run тут
+Настройки для конфигов тут
--- bp_example/my_app/run.py
+++ bp_example/my_app/run.py
@@ -1,5 +1,19 @@
-from my_app import app
+import os, sys
+sys.path.append(os.path.pardir)
+from my_app import create_app
+from my_app.config import ProductionConfig
 
+
+app = create_app
+# # print(app.config)  #конфигурация по умолчанию
+# app.config.from_object("my_app.config.DevelopmentConfig")
+
+# print("\n === After loading config === \n")
+# print(app.config) #Словарь настроек для запуска приложения flask
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        port=ProductionConfig.PORT, 
+        host=ProductionConfig.SERVER_NAME, 
+        debug=ProductionConfig.DEBUG
+        )
