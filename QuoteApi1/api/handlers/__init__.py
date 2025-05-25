d37b77a114d9605d32eb0d40c7691ab0d3303cc8
Sk8alex <excellentforme@gmail.com>
2025-05-25 15:37:36 +0300

    error

---
+++ QuoteApi1/api/handlers/__init__.py
@@ -0,0 +1,9 @@
+from api import app
+from flask import jsonify
+from werkzeug.exceptions import HTTPException
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """ Функция для перехвата HTTP ошибок и возврата в виде JSON."""
+    return jsonify({"error": str(e)}), e.code
