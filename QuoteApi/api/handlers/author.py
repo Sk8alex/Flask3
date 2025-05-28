a0c492f8b150a593d1ad1a1b27da87e7540251ff
Sk8alex <excellentforme@gmail.com>
2025-05-28 15:36:14 +0300

    commit QA

--- QuoteApi/api/handlers/author.py
+++ QuoteApi/api/handlers/author.py
@@ -19,20 +19,33 @@ def create_author():
     return jsonify(author.to_dict()), 201
 
 
-# URL: "/authors/<int:author_id>/quotes"
-@app.route("/authors/<int:author_id>/quotes", methods=["GET", "POST"])
-def author_quotes(author_id: int):
+# # URL: "/authors/<int:author_id>/quotes"
+# @app.route("/authors/<int:author_id>/quotes", methods=["GET", "POST"])
+# def author_quotes(author_id: int):
+#     author = db.get_or_404(AuthorModel, author_id, description=f"Author with id={author_id} not found")
+
+#     if request.method == "GET":
+#         quotes = [quote.to_dict() for quote in author.quotes]
+#         return jsonify({"author": author.name} | {"quotes": quotes}), 200
+
+#     elif request.method == "POST":
+#         data = request.json
+#         new_quote = QuoteModel(author, **data)
+#         db.session.add(new_quote)
+#         db.session.commit()
+#         return jsonify(new_quote.to_dict() | { "author_id" : author.id}), 201
+#     else:
+#         abort(405)
+
+@app.get("/authors")
+def get_authors():
+    authors_db = db.session.scalars(db.select(AuthorModel)).all()
+    authors = [author.to_dict() for author in authors_db]
+    return jsonify(authors), 200
+
+
+@app.get('/authors/<int:author_id>')
+def get_author_by_id(author_id: int):
     author = db.get_or_404(AuthorModel, author_id, description=f"Author with id={author_id} not found")
-
-    if request.method == "GET":
-        quotes = [quote.to_dict() for quote in author.quotes]
-        return jsonify({"author": author.name} | {"quotes": quotes}), 200
-
-    elif request.method == "POST":
-        data = request.json
-        new_quote = QuoteModel(author, **data)
-        db.session.add(new_quote)
-        db.session.commit()
-        return jsonify(new_quote.to_dict() | { "author_id" : author.id}), 201
-    else:
-        abort(405)
+    # instance -> dict -> json
+    return jsonify(author.to_dict()), 200
