578f55181490dbaca73fbfc5ca63b7eac55f2d11
Sk8alex <excellentforme@gmail.com>
2025-06-08 12:38:09 +0300

    Add users handlers

---
+++ QuoteApi/api/handlers/user.py
@@ -0,0 +1,46 @@
+from api import app, db
+from api.models.user import UserModel
+from flask import abort, jsonify, request
+from api.schemas.user import user_shema
+
+
+
+@app.get('/users/<int:user_id>') #зефирка
+def get_user_by_id(user_id: int):
+    user = db.get_or_404(UserModel, user_id, description=f"User with id={user_id} not found")
+    return jsonify(user_shema.dump(user)),200
+
+
+# def get_user_by_id(user_id: int):
+#     pass
+
+
+@app.get("/users")
+def get_users(): #зефирка
+    users = db.session.scalars(db.select(UserModel)).all()
+    return jsonify(user_shema.dump(users, many=True)), 200
+
+
+
+# def get_users():users
+#     pass
+
+@app.post("/users")
+def create_user():
+    """ Function creates new quote and adds it to db."""
+    data = request.json
+    try:
+        user = UserModel(**data)
+        db.session.add(user)
+        db.session.commit()
+    except TypeError:
+        abort(400, f"Invalid data. Required: <username> and <password>. Received: {', '.join(data.keys())}")
+    except Exception as e:
+        abort(503, f"Database error: {str(e)}")
+    
+    return jsonify(user_shema.dump(user)), 201
+
+
+# def create_user():
+#     pass
+
---
+++ QuoteApi/api/models/user.py
@@ -0,0 +1,45 @@
+from passlib.apps import custom_app_context as pwd_context
+import sqlalchemy.orm as so
+from api import db
+import sqlalchemy as sa
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from flask import abort, jsonify, request
+
+class UserModel(db.Model):
+    __tablename__="users"
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(32), index=True,
+    unique=True)
+    password_hash: so.Mapped[str] = so.mapped_column(sa.String(128))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.hash_password(password)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+        
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+    
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit
+        except IntegrityError as e:
+            db.session.rollback()
+            abort(400, f"Database intergrety error: {str(e)}")
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            abort(503, f"Database error: {str(e)}")
+
+    
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            abort(503, f"Database error: {str(e)}")
